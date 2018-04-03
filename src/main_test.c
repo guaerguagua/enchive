79bca8ecebf0837f5198836c458892c0b080c624
guaerguagua <1025191631@qq.com>
2018-04-03 16:37:29 +0800

    debug

--- src/main_test.c
+++ src/main_test.c
@@ -26,25 +26,42 @@ main(int argc, char **argv)
     **/
 
     printf("start test sha256\n");
-    u8 in[100];
+
     u8 out[SHA256_BLOCK_SIZE*2+1];
     while(1) {
         printf("please input a string:\n");
+        u8 in[100];
         scanf("%s", in);
+        if(strcmp("next",in)){
+            break;
+        }
         printf("in  string:%s\n", in);
         int inLen = strlen(in);
         printf("in     len:%d\n", inLen);
         sha256(in, inLen, out, sizeof(out)-1);
         printf("out string:%s\n", out);
-        printf("out    len:%d\n", strlen(out)-1);
+        printf("out    len:%d\n", strlen(out));
         u8 salt[SHA256_SALT_SIZE+1]={0};
         generateSalt(salt, sizeof(salt)-1);
-        printf("salt:%s\nsalt len:%d\n",salt, sizeof(salt)-1);
+        printf("salt string:%s\n",salt);
+        printf("salt    len:%d\n",strlen(salt));
         sha256_salt(in,inLen,out,sizeof(out)-1,salt,sizeof(salt)-1);
         printf("out with salt string:%s\n",out);
-        printf("out with salt    len:%d\n",sizeof(out)-1);
-
-
+        printf("out with salt    len:%d\n",strlen(out));
     }
+
+    int i =0;
+    struct timespec start = {0, 0};
+    u8 testSrc[15]="sha256seed";
+    u8 testSalt[SHA256_SALT_SIZE+1]={0};
+    generateSalt(testSalt, sizeof(testSalt)-1);
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    u8 testOut[SHA256_BLOCK_SIZE*2];
+    for(i;i<2000;i++){
+        sha256_salt(testSrc, strlen(testSrc),testOut,strlen(testOut),testSalt,strlen(testSalt));
+    }
+    struct timespec end = {0, 0};
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    printf("spend time:%dns",end.tv_nsec-start.tv_nsec);
     return 0;
 }
