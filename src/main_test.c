1b8fc6124de476eef3771edeeb034c9087046ea8
guaerguagua <1025191631@qq.com>
2018-04-03 14:17:08 +0800

    debug

--- src/main_test.c
+++ src/main_test.c
@@ -13,14 +13,14 @@ main(int argc, char **argv)
 {
     struct timespec time1 = {0, 0};
     clock_gettime(CLOCK_REALTIME, &time1);
-    printf("CLOCK_REALTIME: %d, %d", time1.tv_sec, time1.tv_nsec);
+    printf("CLOCK_REALTIME: %d, %d\n", time1.tv_sec, time1.tv_nsec);
     clock_gettime(CLOCK_MONOTONIC, &time1);
-    printf("CLOCK_MONOTONIC: %d, %d", time1.tv_sec, time1.tv_nsec);
+    printf("CLOCK_MONOTONIC: %d, %d\n", time1.tv_sec, time1.tv_nsec);
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-    printf("CLOCK_PROCESS_CPUTIME_ID: %d, %d", time1.tv_sec, time1.tv_nsec);
+    printf("CLOCK_PROCESS_CPUTIME_ID: %d, %d\n", time1.tv_sec, time1.tv_nsec);
     clock_gettime(CLOCK_THREAD_CPUTIME_ID, &time1);
-    printf("CLOCK_THREAD_CPUTIME_ID: %d, %d", time1.tv_sec, time1.tv_nsec);
-    printf("\n%d\n", time(NULL));
+    printf("CLOCK_THREAD_CPUTIME_ID: %d, %d\n", time1.tv_sec, time1.tv_nsec);
+    printf("%d\n", time(NULL));
     sleep(1);
 
     printf("start test sha256\n");
